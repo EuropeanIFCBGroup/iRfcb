f877689137146ef17ce9efb26d2871d59bd71bf2
anderstorstensson <anders.torstensson@smhi.se>
2024-07-16 19:22:44 +0200

    improve function

--- R/ifcb_get_trophic_type.R
+++ R/ifcb_get_trophic_type.R
@@ -1,17 +1,21 @@
 utils::globalVariables(c("scientific_name", "n_distinct", "trophic_type"))
 
-#' @title Get Trophic Type for a List of Taxa
-#' @description This function reads a file containing trophic types for various taxa (originating from SMHI),
-#'              and returns the trophic type for a specified list of taxa.
-#'              If there are multiple unique trophic types for a scientific name,
-#'              the summarized trophic type is "NS".
+#' @title Get Trophic Type for a List of Plankton Taxa
+#' @description This function matches a specified list of taxa with a summarized list of trophic types
+#'              for various plankton taxa from Northern Europe (data sourced from `SMHI Trophic Type`).
 #'
 #' @param taxa_list A character vector of scientific names for which trophic types are to be retrieved.
+#' @param print_complete_list Logical, if TRUE, prints the complete list of summarized trophic types.
 #'
 #' @importFrom dplyr group_by summarize filter %>% n_distinct
 #' @importFrom readr read_delim
 #'
-#' @return A character vector of trophic types corresponding to the scientific names in \code{taxa_list}.
+#' @details
+#' If there are multiple trophic types for a scientific name (i.e. AU and HT size classes),
+#' the summarized trophic type is "NS".
+#'
+#' @return A character vector of trophic types corresponding to the scientific names in \code{taxa_list},
+#' or a data frame containing all taxa and trophic types available in the `SMHI Trophic Type` list.
 #'
 #' @examples
 #' # Example usage:
@@ -24,7 +28,21 @@ utils::globalVariables(c("scientific_name", "n_distinct", "trophic_type"))
 #' ifcb_get_trophic_type(taxa_list)
 #'
 #' @export
-ifcb_get_trophic_type <- function(taxa_list) {
+ifcb_get_trophic_type <- function(taxa_list = NULL, print_complete_list = FALSE) {
+
+  # Input validation using base R
+  if (!is.null(taxa_list) && !is.character(taxa_list)) {
+    stop("Error: taxa_list must be a character vector.")
+  }
+
+  if (!is.logical(print_complete_list)) {
+    stop("Error: print_complete_list must be a logical value.")
+  }
+
+  if (!is.null(taxa_list) && print_complete_list) {
+    warning("Both taxa_list and print_complete_list are provided. Only the taxa_list results will be returned.")
+  }
+
   # Read the TSV file into a data frame
   df <- read_delim(system.file("exdata/trophictype_smhi.txt", package = "iRfcb"), show_col_types = FALSE)
 
@@ -34,17 +52,25 @@ ifcb_get_trophic_type <- function(taxa_list) {
     summarize(trophic_type = ifelse(n_distinct(trophic_type) > 1, "NS", unique(trophic_type)),
               .groups = 'drop')
 
-  # Create a data frame to hold the result
-  result <- data.frame(scientific_name = taxa_list, trophic_type = NA, stringsAsFactors = FALSE)
+  if (!is.null(taxa_list)) {
 
-  # Match scientific_name with the specified list of taxa and return the trophic type
-  for (i in 1:nrow(result)) {
-    match <- summarized_df %>%
-      filter(scientific_name == result$scientific_name[i])
-    if (nrow(match) > 0) {
-      result$trophic_type[i] <- match$trophic_type
+    result <- data.frame(scientific_name = taxa_list, trophic_type = NA, stringsAsFactors = FALSE)
+
+    # Match scientific_name with the specified list of taxa and return the trophic type
+    for (i in 1:nrow(result)) {
+      match <- summarized_df %>%
+        filter(scientific_name == result$scientific_name[i])
+      if (nrow(match) > 0) {
+        result$trophic_type[i] <- match$trophic_type
+      }
     }
+    output <- result$trophic_type
+
+  } else if (print_complete_list) {
+    output <- summarized_df
+  } else {
+    stop("Error: No valid input provided. Please specify either taxa_list or set print_complete_list to TRUE.")
   }
 
-  return(result$trophic_type)
+  return(output)
 }
