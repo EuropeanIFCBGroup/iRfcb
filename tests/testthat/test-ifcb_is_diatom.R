b87e64113b5d230a14926a761492d051daa689af
anderstorstensson <anders.torstensson@smhi.se>
2024-08-27 21:28:23 +0200

    add multiple attemps to api call

--- tests/testthat/test-ifcb_is_diatom.R
+++ tests/testthat/test-ifcb_is_diatom.R
@@ -63,5 +63,5 @@ test_that("ifcb_is_diatom handles errors from wm_records_names gracefully", {
   taxa_list <- c("Nitzschia_sp", "Chaetoceros_sp", "Dinophysis_norvegica", "Thalassiosira_sp")
 
   # Call the function and check for error handling
-  expect_error(ifcb_is_diatom(taxa_list), "Error occurred while retrieving worms records: API error")
+  expect_error(ifcb_is_diatom(taxa_list), "Error occurred while retrieving worms records after 3 attempts: API error")
 })
