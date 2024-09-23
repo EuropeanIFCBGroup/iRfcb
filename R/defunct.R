f23bfd9d578df1bfccf33206a6ad6f406e692fa6
anderstorstensson <anders.torstensson@smhi.se>
2024-09-23 09:08:20 +0200

    add defunct

---
+++ R/defunct.R
@@ -0,0 +1,23 @@
+#' Defunct functions
+#'
+#' @description
+#' `r lifecycle::badge("defunct")`
+#'
+#' These functions were deprecated for at least five release cycles before being
+#' made defunct. If there's a known replacement, calling the function
+#' will tell you about it.
+#'
+#' @keywords internal
+#' @name defunct
+NULL
+
+#' @usage # Deprecated in 0.3.* -------------------------------------
+#' @name defunct
+NULL
+
+#' @export
+#' @rdname defunct
+#' @importFrom lifecycle deprecate_stop
+ifcb_get_svea_position <- function(...) {
+  lifecycle::deprecate_stop("0.3.4", "ifcb_get_svea_position()", "iRfcb::ifcb_get_ferrybox_data()")
+}
