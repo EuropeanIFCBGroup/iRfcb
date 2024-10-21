b027bd43016114d6f26e79c8fa7c2ed8c2a5dd9f
anderstorstensson <anders.torstensson@smhi.se>
2024-10-21 13:54:14 +0200

    add function to create class2use file

---
+++ R/ifcb_create_class2use.R
@@ -0,0 +1,33 @@
+utils::globalVariables("save_class2use_to_mat")
+#' Create a class2use .mat File
+#'
+#' This function creates a `.mat` file containing a character vector of class names.
+#' A class2use file can be used for manual annotation using the code in the `ifcb-analysis`
+#' repository (Sosik and Olson 2007).
+#'
+#' @param classes A character vector of class names to be saved in the `.mat` file.
+#' @param filename A string specifying the output file path (with `.mat` extension).
+#' @param do_compression A logical value indicating whether to compress the `.mat` file. Defaults to `TRUE`.
+#'
+#' @importFrom reticulate source_python
+#' @export
+#'
+#' @seealso \code{\link{ifcb_py_install}} \url{https://github.com/hsosik/ifcb-analysis}
+#'
+#' @references Sosik, H. M. and Olson, R. J. (2007), Automated taxonomic classification of phytoplankton sampled with imaging-in-flow cytometry. Limnol. Oceanogr: Methods 5, 204–216.
+#'
+#' @examples
+#' \dontrun{
+#' # Example usage:
+#' classes <- c("unclassified", "Dinobryon_spp", "Helicostomella_spp")
+#'
+#' ifcb_create_class2use(classes, "class2use_output.mat", do_compression = TRUE)
+#' }
+ifcb_create_class2use <- function(classes, filename, do_compression = TRUE) {
+
+  # Source the Python function
+  source_python(system.file("python", "save_class2use_to_mat.py", package = "iRfcb"))
+
+  # Call the function in R
+  save_class2use_to_mat(filename, classes, do_compression)
+}
