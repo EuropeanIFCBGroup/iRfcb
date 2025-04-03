971368c717a8c43890f1e1ad3a722b5e0eec5473
anderstorstensson <anders.torstensson@smhi.se>
2025-04-03 14:30:15 +0200

    defunct ifcb_summarize_png_data

--- tests/testthat/test-defunct.R
+++ tests/testthat/test-defunct.R
@@ -1,3 +1,7 @@
 test_that("ifcb_get_svea_position is defunct", {
   expect_error(ifcb_get_svea_position())
 })
+
+test_that("ifcb_summarize_png_data is defunct", {
+  expect_error(ifcb_summarize_png_data())
+})
