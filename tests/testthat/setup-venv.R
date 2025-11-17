5869ddab4bfa0e6a97ddb3cd420f39b0ae4c82f2
anderstorstensson <anders.torstensson@smhi.se>
2025-11-17 13:40:30 +0100

    explicitly use python path

--- tests/testthat/setup-venv.R
+++ tests/testthat/setup-venv.R
@@ -6,11 +6,14 @@ not_cran <- identical(Sys.getenv("NOT_CRAN"), "true")
 if (not_cran) {
   # Define the virtual environment path
   venv_dir <- file.path(tempdir(), "test-venv")
+  python_path <- Sys.which("python3")
 
   # Use an isolated virtual environment for tests
   if (!reticulate::virtualenv_exists(venv_dir)) {
     message("Creating temporary Python virtual environment for tests...")
-    reticulate::virtualenv_create(venv_dir, requirements = system.file("python", "requirements.txt", package = "iRfcb"))
+    reticulate::virtualenv_create(venv_dir,
+                                  python = python_path,
+                                  requirements = system.file("python", "requirements.txt", package = "iRfcb"))
   }
 
   # Activate the virtual environment
